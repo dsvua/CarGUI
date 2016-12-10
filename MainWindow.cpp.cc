98c4de5ba85ee4f2f1b6fc9ee566021ae01f26b7
serhiy <serhiy@home-linux>
2016-12-10 10:10:05 -0500

    i2c

--- MainWindow.cpp.cc
+++ MainWindow.cpp.cc
@@ -218,7 +218,15 @@ void MainWindow::mouseMoveEvent(QMouseEvent* event){
 }
 
 void MainWindow::wheelEvent(QWheelEvent* event){
-    
+    int numDegrees = event->delta();
+    qDebug() << "numDegrees" << numDegrees << "\n";
+    if (numDegrees > 0){
+        sendMessage("Forward:200");
+    }
+    if (numDegrees < 0){
+        sendMessage("Backward:200");
+    }    
+    event->ignore();
 }
 
 void MainWindow::readyRead(){
